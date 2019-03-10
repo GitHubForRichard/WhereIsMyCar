2e98bcf76cc0a81d2491deebb83a6ff3788a7031
Jackbui96 <nguyenbui1996@gmail.com>
2019-03-10 15:41:02 -0700

    restructure HomepageActivity.java, update Google Services

--- app/src/main/java/com/example/richa/buttonclickapp/HomepageActivity.java
+++ app/src/main/java/com/example/richa/buttonclickapp/HomepageActivity.java
@@ -6,56 +6,49 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
 
-public class HomepageActivity extends AppCompatActivity {
+public class HomepageActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageButton buttonAccount;
     private ImageButton buttonSearch;
-    private ImageButton buttonMap;
-
+    private ImageButton buttonMaps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
 
+        // Initialize UI components, and its functionality
+        initializeUI();
+    }
+
+    private void initializeUI() {
         buttonAccount = findViewById(R.id.imgButton_account);
         buttonSearch = findViewById(R.id.imgButton_search);
-        buttonMap = findViewById(R.id.imgButton_maps);
+        buttonMaps = findViewById(R.id.imgButton_maps);
+        buttonAccount.setOnClickListener(this);
+        buttonSearch.setOnClickListener(this);
+        buttonMaps.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        // Button for account page
+        if (i == R.id.imgButton_account) {
+            Intent intent = new Intent(getApplicationContext(), AccountActivity.class);
+            startActivity(intent);
+        }
+        // Button for search implementation
+        else if (i == R.id.imgButton_search) {
+            Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
+            startActivity(intent);
+        }
+        /**
+         * Todo: Implement Google Maps functionality (Assign: Jackbui96)
+         */
+        // Button for Google Maps
+        else if (i == R.id.imgButton_maps) {
 
-        View.OnClickListener buttomBarListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if(v == buttonAccount) {
-                    Intent intent = new Intent(getApplicationContext(), AccountActivity.class);
-                    startActivity(intent);
-                }
-
-                else if(v == buttonSearch)
-                {
-                    Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
-                    startActivity(intent);
-                }
-
-                else if(v == buttonMap)
-                {
-                    // go to map page...
-                }
-            }
-        };
-
-        buttonAccount.setOnClickListener(buttomBarListener);
-        buttonSearch.setOnClickListener(buttomBarListener);
-        buttonMap.setOnClickListener(buttomBarListener);
-
-//        buttonAccount.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//                Intent intent = new Intent(getApplicationContext(),AccountActivity.class);
-//                startActivity(intent);
-//            }
-//        });
+        }
     }
 }
